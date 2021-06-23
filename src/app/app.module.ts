7ddc0b192a9e848ac9253499d745168e403323fa
henri <hkengoum@gmail.com>
2021-06-23 15:11:10 +0200

    bonjout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     // import HttpClientModule after BrowserModule.
+     HttpClientModule,
+   
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
